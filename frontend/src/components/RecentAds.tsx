bb7f5de2347c765b834c3c937af5b33ca58681d0
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-03-25 17:20:04 +0100

    API created + communication front/back = OK

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -4,55 +4,19 @@ import AdCard, { AdCardProps } from "./AdCard";
 import { useEffect, useState } from "react";
 
 const RecentAds = () => {
-  const ads: AdCardProps[] = [
-    {
-      title: "Table",
-      picture: "/images/table.webp",
-      price: 120,
-      link: "/ads/table"
-    },
-    {
-      title: "Dame-jeanne",
-      picture: "/images/dame-jeanne.webp",
-      price: 75,
-      link: "/ads/dame-jeanne"
-    },
-    {
-      title: "Vide-poche",
-      picture: "/images/vide-poche.webp",
-      price: 4,
-      link: "/ads/vide-poche"
-    },
-    {
-      title: "Vaisselier",
-      picture: "/images/vaisselier.webp",
-      price: 900,
-      link: "/ads/vaisselier"
-    },
-    {
-      title: "Bougie",
-      picture: "/images/bougie.webp",
-      price: 8,
-      link: "/ads/bougie"
-    },
-    {
-      title: "Porte-magazine",
-      picture: "/images/porte-magazine.webp",
-      price: 45,
-      link: "/ads/porte-magazine"
-    }
-  ];
   useEffect(() => {
     const fetchData = async () => { 
       try {
-        const result = await axios.get("http://localhost:3000/ads"); 
-        console.log(result.data)
+        const result = await axios.get<AdCardProps[]>("http://localhost:3000/ads"); 
+        setAds(result.data)
       } catch (error) {
         console.log(error)
       }
     }
     fetchData();
   }, [])
+
+  const [ads, setAds] = useState<AdCardProps[]>([]);
 
   const [total, setTotal] = useState(0); 
   const removePrice = (ad:AdCardProps) => {
