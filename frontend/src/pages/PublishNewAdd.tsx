bf4e4f9c5abeb7a1a94f6379aa9ba926312a9967
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-03-26 15:28:46 +0100

    react hook form added + creation of form

--- frontend/src/pages/PublishNewAdd.tsx
+++ frontend/src/pages/PublishNewAdd.tsx
@@ -1,12 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Categories = {
     id : number; 
     title : string;
 }
 
+type inputTypes = {
+    title : string; 
+    description : string ; 
+    owner : string; 
+    price : number; 
+    picture : string; 
+    location : string; 
+    createdAt : Date;
+    category_id : number; 
+}
+
 const PublishNewAdd = () => {
+    // To show the different categories and fetch them from the BD
     const [categories, setCategories] = useState<Categories[]>([]); 
     useEffect(()=>{
         const fetchCategories = async () => {
@@ -16,40 +29,42 @@ const PublishNewAdd = () => {
         fetchCategories(); 
     },[])
 
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm<inputTypes>()
+    const onSubmit: SubmitHandler<inputTypes> = (data) => console.log(data)
+
+    const img = "https://cdn.pixabay.com/photo/2016/03/05/19/51/easter-1238537_1280.jpg"; 
+
     return(
         <main className="main-content">
             <h2>Publish new add! </h2>
             <div >
-            <form onSubmit={(e)=> {
-                // Prevent the browser from reloading the page
-                e.preventDefault();
-
-                // Read the form Data
-                const form = e.target;
-                const formData = new FormData(form as HTMLFormElement) ;
-
-                const formJson = Object.fromEntries(formData.entries());
-                console.log(formJson);
-
-            }} 
+            <form onSubmit={handleSubmit(onSubmit)}
             style={{display: "flex", flexDirection: "column", gap: "2rem", justifyContent: "center", alignItems: "flex-start"}}>
             <label>Title
-                <input type="text" name="title"></input>
+                <input type="text" {...register("title", {required:true})}></input>
             </label>
             <label>Description
-                <textarea name="description"></textarea>
+                <textarea defaultValue={"This item is the best !"}{...register("description")}></textarea>
             </label>
             <label>Owner
-                <input type="text" name="owner"></input>
+                <input type="text" {...register("owner", {required:true})}></input>
+            </label>
+            <label>Add an image
+                <input type="text" defaultValue={img} {...register("picture")}></input>
             </label>
             <label>Price
-                <input type="number" name="price"></input>
+                <input type="number" {...register("price", {required:true})}></input>
             </label>
             <label>Location
-                <input type="text" name="location"></input>
+                <input type="text" {...register("location", {required: true})}></input>
             </label>
             <label>Date of creation
-                <input type="date" name="createdAt"></input>
+                <input type="date" {...register("createdAt", {required:true })}></input>
             </label>
             <select name="category_id">
                 {categories.map(item => 
