7e975b4dc304d19e1e3d171d0a750f03591f6ccf
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-03-26 15:35:43 +0100

    react-hook-form => connexion backend done and working

--- frontend/src/pages/PublishNewAdd.tsx
+++ frontend/src/pages/PublishNewAdd.tsx
@@ -32,10 +32,11 @@ const PublishNewAdd = () => {
     const {
         register,
         handleSubmit,
-        watch,
+        // watch,
         formState: { errors },
       } = useForm<inputTypes>()
-    const onSubmit: SubmitHandler<inputTypes> = (data) => console.log(data)
+
+    const onSubmit: SubmitHandler<inputTypes> = async (data) => await axios.post("http://localhost:3000/ads", data); 
 
     const img = "https://cdn.pixabay.com/photo/2016/03/05/19/51/easter-1238537_1280.jpg"; 
 
