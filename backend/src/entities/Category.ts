9509e09d49015a09292514366bfbf55d1b94df3b
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-06-27 11:32:02 +0200

    I lost the thread

--- backend/src/entities/Category.ts
+++ backend/src/entities/Category.ts
@@ -6,16 +6,21 @@ import {
   PrimaryGeneratedColumn,
 } from "typeorm";
 import Ad from "./Ad";
+import { Field, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 class Category extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   @Column({ unique: true })
+  @Field()
   title: string;
 
   @OneToMany(() => Ad, (ad) => ad.category)
+  @Field(() => [Ad])
   ads: Ad[];
 }
 
--- backend/src/entities/Tag.ts
+++ backend/src/entities/Tag.ts
@@ -6,16 +6,21 @@ import {
   PrimaryGeneratedColumn,
 } from "typeorm";
 import Ad from "./Ad";
+import { Field, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 class Tag extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
+  @Field()
   title: string;
 
-  @ManyToMany(() => Ad)
+  @ManyToMany(() => Ad, (ad) => ad.tags)
+  @Field(() => [Ad])
   ads: Ad[];
 }
 
